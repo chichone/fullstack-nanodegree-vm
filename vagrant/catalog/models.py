97a385a861cd039f511c9a750cde196bf53eace6
chichone <cakyle@gmail.com>
2018-01-25 16:31:28 -0800

    Add get users api

--- vagrant/catalog/models.py
+++ vagrant/catalog/models.py
@@ -1,8 +1,28 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine
+from sqlalchemy.orm import relationship, sessionmaker
+from passlib.apps import custom_app_context as pwd_context
 
 Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(32), index=True)
+    password_hash = Column(String(64))
+
+    def hash_password(self, password):
+        self.password_hash = pwd_context.encrypt(password)
+
+    def verify_password(self, password):
+        return pwd_context.verify(password, self.password_hash)
+
+    @property
+    def serialize(self):
+      return {
+          'id': self.id,
+           'name': self.username,
+       }
 
 ##Create an item class for our database
 class Item(Base):
@@ -20,8 +40,6 @@ class Item(Base):
            'name': self.name,
            'description' : self.description
        }
-
-
 
 engine = create_engine('sqlite:///items.db')
 Base.metadata.create_all(engine)
--- vagrant/catalog/views.py
+++ vagrant/catalog/views.py
@@ -1,7 +1,10 @@
-from flask import Flask, request, jsonify
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from models import Base, Item
+from models import Base, Item, User
+from flask import Flask, jsonify, request, url_for, abort, g
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+from flask.ext.httpauth import HTTPBasicAuth
+auth = HTTPBasicAuth()
 
 
 engine = create_engine('sqlite:///items.db')
@@ -29,6 +32,33 @@ def itemsFunction():
         # return 'hi'
         return makeANewItem(name, description)
 
+@app.route('/users', methods = ['POST'])
+def new_user():
+    username = request.json.get('username')
+    password = request.json.get('password')
+    if username is None or password is None:
+        print "missing arguments"
+        abort(400)
+
+    if session.query(User).filter_by(username = username).first() is not None:
+        print "existing user"
+        user = session.query(User).filter_by(username=username).first()
+        return jsonify({'message':'user already exists'}), 200#, {'Location': url_for('get_user', id = user.id, _external = True)}
+
+    user = User(username = username)
+    user.hash_password(password)
+    session.add(user)
+    session.commit()
+    return jsonify({ 'username': user.username }), 201#, {'Location': url_for('get_user', id = user.id, _external = True)}
+
+@app.route('/api/users')
+def get_users():
+    try:
+        users = session.query(User).all()
+    except:
+        return ('no users')
+    return jsonify(users=[user.serialize for user in users])
+
 
 def getAllItems():
     items = session.query(Item).all()
@@ -42,5 +72,5 @@ def makeANewItem(name, description):
     return jsonify(Item=item.serialize)
 
 if __name__ == '__main__':
-        app.debug = False
+        app.debug = True
         app.run(host='0.0.0.0', port=5000)
